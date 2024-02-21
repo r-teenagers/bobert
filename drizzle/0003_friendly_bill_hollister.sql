19a268ab4912a313c735d97af4554bb0195b4dda
Andrew Brower <bigspeedfpv@gmail.com>
2024-02-21 17:39:12 -0500

    fix: fill 0003 migration

--- drizzle/0003_friendly_bill_hollister.sql
+++ drizzle/0003_friendly_bill_hollister.sql
@@ -1,16 +1,7 @@
-/*
- SQLite does not support "Set default to column" out of the box, we do not generate automatic migration for that, so it has to be done manually
- Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
-                  https://www.sqlite.org/lang_altertable.html
-                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
-
- Due to that we don't generate migration automatically and it has to be done manually
-*/--> statement-breakpoint
-/*
- SQLite does not support "Set not null to column" out of the box, we do not generate automatic migration for that, so it has to be done manually
- Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
-                  https://www.sqlite.org/lang_altertable.html
-                  https://stackoverflow.com/questions/2083543/modify-a-columns-type-in-sqlite3
-
- Due to that we don't generate migration automatically and it has to be done manually
-*/
+ALTER TABLE "players" RENAME excluded TO excluded_old;
+--> statement-breakpoint
+ALTER TABLE "players" ADD excluded boolean not null default false;
+--> statement-breakpoint
+UPDATE "players" SET excluded = coalesce(excluded_old, false);
+--> statement-breakpoint
+ALTER TABLE "players" DROP COLUMN excluded_old;
