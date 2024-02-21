69df6d59b2ab15962e43d73393517df57122ff73
Andrew Brower <bigspeedfpv@gmail.com>
2024-02-21 17:40:02 -0500

    fix: fill 0003 migration

--- drizzle/0003_friendly_bill_hollister.sql
+++ drizzle/0003_friendly_bill_hollister.sql
@@ -1,7 +1,7 @@
-ALTER TABLE "players" RENAME excluded TO excluded_old;
+ALTER TABLE "player" RENAME excluded TO excluded_old;
 --> statement-breakpoint
-ALTER TABLE "players" ADD excluded boolean not null default false;
+ALTER TABLE "player" ADD excluded boolean not null default false;
 --> statement-breakpoint
-UPDATE "players" SET excluded = coalesce(excluded_old, false);
+UPDATE "player" SET excluded = coalesce(excluded_old, false);
 --> statement-breakpoint
-ALTER TABLE "players" DROP COLUMN excluded_old;
+ALTER TABLE "player" DROP COLUMN excluded_old;
